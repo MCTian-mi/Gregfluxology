c22f48c8fa8c7a85c2f64e373c8d1b75cd9d0aa6
cr3eperall <38960200+cr3eperall@users.noreply.github.com>
2025-09-03 12:45:58 +0200

    fix divided by zero crash in FEToEUProvider

--- src/main/java/gregfluxology/cap/FEToEUProvider.java
+++ src/main/java/gregfluxology/cap/FEToEUProvider.java
@@ -78,6 +78,7 @@ public class FEToEUProvider extends CapabilityCompatProvider {
             long maxIn = maxReceive / FeCompat.ratio(true);
             long missing = energyContainer.getEnergyCanBeInserted();
             long voltage = energyContainer.getInputVoltage();
+            if(voltage == 0) return 0;
             maxIn = Math.min(missing, maxIn);
             long maxAmp = Math.min(energyContainer.getInputAmperage(), maxIn / voltage);
 
